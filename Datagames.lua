5c024a65be3b622f337a1dfba41b6353d5258939
BayronLawful <125753967+BayronLawful@users.noreply.github.com>
2023-02-20 13:45:47 -0500

    Create Datagames.lua

---
+++ Datagames.lua
@@ -0,0 +1,137 @@
+return {
+    Shindo = {
+        Id = {
+            4616652839,
+            4601350214,
+            4601350394,
+            4601350656,
+            4601350809,
+            5431071837,
+            5447073001,
+            5084678830,
+            5431069982,
+            4601350760,
+            6341670805,
+            6505734854,
+            6901575446,
+            6984568732,
+            6986372023,
+            5451398863,
+            7214033433,
+            5451401540,
+            5307141034,
+            7534339269,
+            5451410109,
+            6593187011,
+            6593188260,
+            5743370338,
+            5664805984,
+            5664803952,
+            8184506020,
+            8472733618,
+            7923764447,
+            9310522814,
+            5451405681,
+            7524809704,
+            7524811367,
+            6602058266,
+            5255237254,
+            6602103757,
+            5824792748,
+            4616652839,
+            4601350214,
+            4601350394,
+            4601350656,
+            4601350809,
+            5431071837,
+            5447073001,
+            5431069982,
+            4601350760,
+            6341670805,
+            6505734854,
+            6901575446,
+            6984568732,
+            6986372023,
+            5451398863,
+            7214033433,
+            5451401540,
+            5307141034,
+            7534339269,
+            5451410109,
+            6593187011,
+            6593188260,
+            5743370338,
+            5664805984,
+            5664803952,
+            8184506020,
+            8472733618,
+            7923764447,
+            9310522814,
+            5451405681,
+            7524809704,
+            7524811367,
+            6602058266,
+            5255237254,
+            6602103757,
+            5824792748
+        },
+        Data = {
+            Maintenance = false,
+            Reason = "Adding a new key sys pls be patient",
+            Name = "Shindo Life",
+            Link = "https://raw.githubusercontent.com/BayronLawful/PlatinumHubV2/main/Shindo%20Life"
+        }
+    },
+    Pnw = {
+        Id = 6918802270,
+        Data = {
+            Maintenance = true,
+            Reason = "Discontinued",
+            Name = "Project New World",
+            Link = "return print('Discontinued')"
+        }
+    },
+    Sfs = {
+        Id = 11040063484,
+        Data = {
+            Maintenance = False,
+            Reason = "Fixing Bug!",
+            Name = "Sword Fighters Simulator",
+            Link = "https://raw.githubusercontent.com/BayronLawful/PlatinumHubV2/main/SwordFighters.lua"
+        }
+    },
+    asou = {
+        Id = 11542692507,
+        Data = {
+            Maintenance = false,
+            Reason = "In Development",
+            Name = "Anime Soul Simulator",
+            Link = "Wait"
+        }
+    },
+    ads = {
+        Id = {
+            6938803436,
+            7338881230,
+            6990129309,
+            6990133340,
+            7274690025,
+            6990131029
+        },
+        Data = {
+            Maintenance = false,
+            Reason = "In Development",
+            Name = "Anime Dimension Sim",
+            Link = "Wait"
+        }
+    },
+    ofs = {
+        Id = 11445923563,
+        Data = {
+            Maintenance = true,
+            Reason = "Coming Soon",
+            Name = "One Fruit",
+            Link = "Wait"
+        }
+    }
+}
